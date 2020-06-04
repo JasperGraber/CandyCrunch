f36aa11b8bbd088d854823d9c8bcbe33fa1196fc
Jasper Gräber <328218@student.mboutrecht.nl>
2020-06-04 12:31:13 +0200

    Navbar 1.0 and Fonts added
    Eerste versie van de navbar en fonts toegevoegd

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Candy Crunch</h1>
+    <p class="lead">American sweets</p>
+  </div>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">CandyCrunch</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./index.php?content=">Winkel</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php?content=brands">Merken</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php?content=company">Over ons</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php?content=login">Inloggen</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php?content=register">Registreren</a>
+      </li>
+    </ul>
+  </div>
+</nav>
