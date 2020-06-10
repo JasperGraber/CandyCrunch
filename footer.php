3d7e884cd4184c89c602b71461811d6d2ffd26d2
Jasper Gräber <328218@student.mboutrecht.nl>
2020-06-10 08:51:58 +0200

    Footer 1.0
    Eerste versie van de footer toegevoegd.

--- footer.php
+++ footer.php
@@ -1,1 +1,16 @@
-footer
+<div class="container-fluid px-0">
+    <div class="row footer">
+        <div class="col-2"></div>
+        <div class="col-3">
+            <h5>Contact informatie</h5><br />
+            <p>Tel: 06-63826579</p>
+            <p>E-mail: contact.candycrunch@gmail.com</p>
+        </div>
+        <div class="col-3">
+            2
+        </div>
+        <div class="col-3">
+            3
+        </div>
+    </div>
+</div>
