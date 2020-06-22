a907c05db2e50d171ff8d1e8b39e21df3f3678ab
Christian Dokter <328370@student.mboutrecht.nl>
2020-06-22 14:03:28 +0200

    Over ons pagina gemaakt style+html

---
+++ company.php
@@ -0,0 +1,87 @@
+<div class="about-section">
+  <h1>Over ons</h1>
+  <p>Wij zijn een een snoepwinkel gevestigd in utrecht wij specializeren ons in amerikaanse lekkernijen.</p>
+  <p>Alles wat u hier op de website ziet kunt u ook in onze winkels vinden dus kom gerust een keer langs.</p>
+  <h2>Contact Informatie</h2>
+    <p>Tel:06-63826579
+    E-mail: contact.candycrunch@gmail.com 
+  </p>
+</div>
+
+<h2 style="text-align:center">ons Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="./img/jasper.jpeg" alt="jasper" width="50%">
+      <div class="container">
+        <h2>Jasper Gräber</h2>
+        <p class="title">CEO</p>
+        <p>Jasper is 17 jaar oud en woont in amersfoort, Utrecht</p>
+        <p>328218@student.mboutrecht.nl</p>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="column">
+    <div class="card">
+      <img src="./img/elon.jpg" alt="elon" style="width:50%">
+      <div class="container">
+        <h2>Christian Dokter</h2>
+        <p class="title">CEO</p>
+        <p>Christian is 18 jaar oud en woont in Vleuten, Utrecht</p>
+        <p>328370@student.mboutrecht.nl</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: middle;
+  width: 50%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #ff5756;
+  color: white;
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+</style>
---
+++ shop.php
@@ -0,0 +1,273 @@
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/LuckyCharms.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">LuckyCharms</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/CinnamonToastCrunch.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Cinnamon Toast Crunch</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/CocoaPuffs.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Cocoa Puffs</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/ReesesPuffs.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Reeses Puffs</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Trix.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Trix</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Cheerios.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Cheerios</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Wheaties.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Wheaties</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/RiceChex.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">RiceChex</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+</hr>
+
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/warheads.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Warheads</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/SwedishFish.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Swedishfish</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/JollyRanchers.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Jollyranchers</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Kool-aidGummies.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Kool-aid Gummies</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Nerds.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Nerds</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Redhots.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Redhots</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Mikeandike.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Mike and Ike</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Twizzlers.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Twizzlers</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+</hr>
+
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Ruffles.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Ruffles</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/HotCheetos.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Hot Cheetos</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Fritos.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Fritos</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Poppables.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Poppables</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/GarlicChips.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Garlic Chips</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/Goldfish.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">GoldFish</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/DoritosDinamita.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Doritos Dinamita</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="./img/PizzaPringles.jpg" alt="" style="width: 100%">
+                <div class="card-body">
+                    <h5 class="card-title">Pizza Pringles</h5>
+                    <p class="card-text">Prijs</p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
